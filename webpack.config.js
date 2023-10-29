31a2d29858bd8d1b84a667c78d2ce46b23e953d5
Diogo Henrique <diogohs110@gmail.com>
2023-10-29 20:44:01 -0300

    Create project structure

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/assets/html/template.html',
+		}),
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist'),
+		},
+		compress: true,
+		port: 3000,
+		open: true,
+	},
+};
